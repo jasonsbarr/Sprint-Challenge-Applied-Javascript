5c407c21e4ebe576b8492fad6c18e5a2f176979c
Jason Barr <jason@jasonsbarr.com>
2019-04-20 14:02:33 -0500

    Register event handlers and set up changing the image with automatic change disabled by hover

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -21,6 +21,39 @@ class Carousel {
 
         this.images[this.current].show();
         this.registerEventHandlers();
+        this.timer = this.autoSlide();
+    }
+
+    registerEventHandlers() {
+        this.buttons.forEach(button => {
+            button.element.addEventListener('click', (event) => {
+                this.slideImage(button.direction);
+            });
+        });
+
+        this.element.addEventListener('mouseenter', event => clearInterval(this.timer));
+        this.element.addEventListener('mouseleave', event => {
+            this.timer = this.autoSlide()
+        });
+    }
+
+    slideImage(direction) {
+        let next;
+        if (direction === 'left') {
+            next = (this.current < this.images.length - 1) ? this.current + 1 : 0;
+        } else {
+            next = (this.current > 0) ? this.current - 1 : this.images.length - 1;
+        }
+
+        this.images[this.current].hide();
+        this.images[next].show();
+        this.current = next;
+    }
+
+    autoSlide() {
+        return setInterval(() => {
+            this.slideImage('left');
+        }, 2000)
     }
 }
 
